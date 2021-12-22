36ae657d1bf8f7910d61ed4fa65094471e638f90
Joongwon Ahn <winterdawnlight@seoul.ac.kr>
2021-12-23 01:06:05 +0900

    solve problem 006

--- problems/006/solve.cpp
+++ problems/006/solve.cpp
@@ -1,5 +1,20 @@
 #include "../../utils/main.h"
 #include <iostream>
+
+constexpr int sum_linear(int n) {
+    return n * (n + 1) / 2;
+}
+
+constexpr int sum_quadratic(int n) {
+    return n * (n + 1) * (2 * n + 1) / 6;
+}
+
+constexpr int square(int n) {
+    return n * n;
+}
+
+#define NUMBER 100
+
 void solve() {
-    std::cout << "not solved" << std::endl;
+    std::cout << square(sum_linear(NUMBER)) - sum_quadratic(NUMBER) << std::endl;
 }
