a98c9ea7965d967285ad7227f04feed45b4a8e70
Joongwon Ahn <winterdawnlight@seoul.ac.kr>
2021-12-22 21:53:42 +0900

    solve problem 001

---
+++ problems/001/main.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Joongwon on 2021-12-22.
+//
+
+#include <iostream>
+#include <chrono>
+
+#define UPPER_LIMIT 1000
+#define PRIME_A 3
+#define PRIME_B 5
+
+constexpr int count_multiple(int base, int maximum) {
+    return maximum / base;
+}
+
+constexpr int sum_multiple_n(int base, int n) {
+    return n * (1 + n) / 2 * base;
+}
+
+constexpr int sum_multiple_below(int base, int upper_limit) {
+    return sum_multiple_n(base, count_multiple(base, upper_limit - 1));
+}
+
+int main(void) {
+    auto time_begin = std::chrono::steady_clock::now();
+    constexpr int answer = sum_multiple_below(PRIME_A, UPPER_LIMIT)
+            + sum_multiple_below(PRIME_B, UPPER_LIMIT)
+            - sum_multiple_below(PRIME_A * PRIME_B, UPPER_LIMIT);
+    auto time_end = std::chrono::steady_clock::now();
+    auto duration = time_end - time_begin;
+
+    std::cout << "answer:" << answer << "\n";
+    std::cout << "time:" << duration.count() << "s\n";
+}
---
+++ problems/001/readme.md
@@ -0,0 +1,13 @@
+#Problem 1: Multiples of 3 or 5
+
+## 문제
+>If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+>
+>Find the sum of all the multiples of 3 or 5 below 1000.
+
+10 미만의 자연수 중에서 3 또는 5의 배수는 3, 5, 6, 9이고, 이들의 합은 23이다.
+
+1000 미만의 3 또는 5의 배수를 모두 더하면 얼마인가?
+
+## 풀이
+등차수열의 합, 포함배제의 원리
